fe093e6ec357474b523315788bc0e3fc6b784665
Aïssa <82447520+AyssKaizen@users.noreply.github.com>
2022-01-03 22:05:13 +0100

    the view up when the keyboard is open

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,45 +1,47 @@
-import React,{useState, useRef} from 'react'
-import { StyleSheet } from 'react-native';
+import React,{useState} from 'react'
+import {View, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback,Keyboard } from 'react-native';
 import HeadingProfile from '../components/profile/HeadingProfile';
-import { ScrollView } from 'react-native';
-import { View } from '../components/Themed';
 import { Divider, Drawer, TextInput,  } from 'react-native-paper';
 import SpeedActions from '../components/profile/SpeedActions';
 export default function ProfileScreen() {
   const [text, setText] = useState("")
-  const scrollRef = useRef(null)
+  const [editable, setEditable] = useState(false)
+
   
   return (
-  <View style={{height:'100%'}} >
-      <HeadingProfile/>
-      <ScrollView ref={scrollRef}>
-        <SpeedActions onPress={()=> console.log('Favoris clicked')} icon='star' title="Favoris" style={styles.fav}/>
-        <SpeedActions onPress={()=> console.log('Chat clicked')} icon='forum' title="Chat" style={styles.chat} />
-        <Divider style={{backgroundColor: '#FA4B7C', height: 0.5}}/>
-        <Drawer.Item 
-          style={{marginLeft: '10%'}}
-          icon='home-variant-outline'
-          label='Hueco mundo'
-        />
-        <Drawer.Item 
-          style={{marginLeft: '10%'}}
-          icon='at'
-          label='nellsama@hueco.com'
-        />
-        <TextInput
-          label='Description'
-          value={text}
-          onChangeText={text => setText(text)}
-          placeholder='votre description'
-          style={{marginHorizontal: 20, height: 120, marginVertical: 50}}
-          multiline
-          numberOfLines={4}
-          autoComplete
-          activeUnderlineColor='#FA4B7C'
-          //onFocus={()=> console.log(scrollRef.current.scrollTo({y:}))}
-        />
-      </ScrollView>
-  </View>
+  <KeyboardAvoidingView behavior='position'>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View >
+          <HeadingProfile/>
+            <SpeedActions onPress={()=> console.log('Favoris clicked')} icon='star' title="Favoris" style={styles.fav}/>
+            <SpeedActions onPress={()=> console.log('Chat clicked')} icon='forum' title="Chat" style={styles.chat} />
+            <Divider style={{backgroundColor: '#FA4B7C', height: 0.5}}/>
+            <Drawer.Item 
+              style={{marginLeft: '10%'}}
+              icon='home-variant-outline'
+              label='Hueco mundo'
+            />
+            <Drawer.Item 
+              style={{marginLeft: '10%'}}
+              icon='at'
+              label='nellsama@hueco.com'
+            />
+            <TextInput
+              label='who i am'
+              value={text}
+              onChangeText={text => setText(text)}
+              placeholder='votre description'
+              style={{marginHorizontal: 20, marginVertical: 20}}
+              multiline
+              numberOfLines={5}
+              autoComplete
+              activeUnderlineColor='#FA4B7C'
+              editable={editable}
+              right={<TextInput.Icon name='pen' onPress={()=> {setEditable(!editable)}}/>}
+            />
+      </View>
+    </TouchableWithoutFeedback>
+  </KeyboardAvoidingView>
   );
 }
 
